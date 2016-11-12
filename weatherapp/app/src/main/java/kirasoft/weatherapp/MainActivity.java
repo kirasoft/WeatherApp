37529a1a0e27ba0eeb888def4b4875811457010f
kota <megatenkota2@gmail.com>
2016-11-11 19:04:26 -0500

    butterknife injections of main activity

--- weatherapp/app/src/main/java/kirasoft/weatherapp/MainActivity.java
+++ weatherapp/app/src/main/java/kirasoft/weatherapp/MainActivity.java
@@ -2,12 +2,40 @@ package kirasoft.weatherapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by dakotajustin on 11/11/16.
  */
 
 public class MainActivity extends Activity implements MainView.RequiredViewOps {
+
+    private MainView.ProvidedPresenterOps presenter;
+
+    @BindView(R.id.edit_text_city)
+    EditText editCity;
+    @BindView(R.id.button_update_weather)
+    Button updateWeather;
+    @BindView(R.id.text_view_instructions)
+    TextView instructions;
+    @BindView(R.id.text_view_weather)
+    TextView weatherText;
+
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
 
     @Override
     public Context getAppContext() {
